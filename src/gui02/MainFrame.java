b5b9371c22ee29d78d7651b0cb8a73e290331805
ravminsk <ravminsk@yandex.ru>
2018-09-28 10:05:53 +0300

    27 09

--- src/gui02/MainFrame.java
+++ src/gui02/MainFrame.java
@@ -1,20 +1,13 @@
 package gui02;
 
-import java.awt.BorderLayout;
 import java.awt.FileDialog;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 class MainFrame extends JFrame {
 	private static final int DEFAULT_WIDTH = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 3);
@@ -32,7 +25,7 @@ class MainFrame extends JFrame {
 
 	// конструктор для инициализации окна
 	public MainFrame(String title) {
-	//	setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		// setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle(title);
 		setLocationByPlatform(true);
@@ -42,34 +35,29 @@ class MainFrame extends JFrame {
 
 		panel4.add(new JScrollPane(txtArea));
 
-		panel5.setLayout(new GridLayout(2,1));
+		panel5.setLayout(new GridLayout(2, 1));
 		panel5.add(panel3);
 		panel5.add(panel4);
 
 		add(panel5);
 		pack();
-	
 
 		bArchive.addActionListener(e -> {
 			FileDialog fd = new FileDialog(this, "Выберите файл для сжатия", FileDialog.LOAD);
 			fd.setVisible(true);
 			String filename = fd.getFile();
 			if (filename != null) {
-				txtArea.append("Выбран файл для сжатия: "+ filename+"\n");
-				
-				
+				txtArea.append("Выбран файл для сжатия: " + fd.getDirectory() + "" + filename + "\n");
 			}
-			
 		});
-		
+
 		bExtract.addActionListener(e -> {
 			FileDialog fd = new FileDialog(this, "Выберите файл для извлечения из архива", FileDialog.LOAD);
 			fd.setVisible(true);
 			String filename = fd.getFile();
-			if (filename != null)	{
-				txtArea.append("Выбран файл для извлечения: "+ filename+"\n");
+			if (filename != null) {
+				txtArea.append("Выбран файл для извлечения: " + fd.getDirectory() + filename + "\n");
 			}
-			
 		});
 
 	}
